69e64fcd6439af481cfb5718fbfa1d97b89b824b
Aleksey Gulidin <89095500+AlekseyHulidzin@users.noreply.github.com>
2022-12-21 16:51:40 +0300

    Update 2.py

--- lesson_2/2.py
+++ lesson_2/2.py
@@ -4,19 +4,16 @@
 # Для n=4 -> [2, 2.25, 2.37, 2.44]
 # Сумма 9.06
 
-number = int(input("Введите список: "))
-sum = 0
-d = {3 * i + 1 for i in range(1,number+1)}
-print(f"Для n = {number} {d}")
+number = int(input("Введите число: "))
+summa = 0
+
+
+def func(num):
+    return [round((1 + 1 / n) ** n, 2) for n in range(1, number + 1)]
+
 
 for i in range(1, number + 1):
+    summa += round(((1 + 1 / i) ** i), 2)
 
-    sum += (1 + 1 / i) ** i
-print(f'Сумма последовательности из {number} чисел равна: {sum}')
-
-def sequence(number):
-
-    return [round((1 + 1 / n)**n, 2) for n in range (1, number + 1)]   
-
-print(f'Для {number} ->',sequence(number))
-
+print(f'Для {number} ->', func(number))
+print(f'Сумма последовательности из {number} чисел равна: {summa}')
