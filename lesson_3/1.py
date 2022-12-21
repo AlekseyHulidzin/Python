7804532807a8c0a8b463b65613193fffaf4adb7c
Aleksey Gulidin <89095500+AlekseyHulidzin@users.noreply.github.com>
2022-12-21 18:48:56 +0300

    Update 1.py

--- lesson_3/1.py
+++ lesson_3/1.py
@@ -11,3 +11,12 @@ for i in range(1, len (result),2):
     if i%2==1:
         summ+=result[i]
 print(summ)
+
+#это если по простому брать уже готовый список
+#my_lst = [2, 3, 5, 9, 3]     
+#result = []
+#for i in range(len(my_lst)):
+#    if i % 2 != 0:
+#        result.append(my_lst[i])
+#summa_el = sum(result)
+#print(f'{my_lst} -> на нечётных позициях элементы {result}, ответ: {summa_el}')
