bcfbdaf7f034fe428ea4abfe1ea8b28df08ad398
Aleksey Gulidin <89095500+AlekseyHulidzin@users.noreply.github.com>
2023-02-05 16:03:56 +0300

    Delete Lesson_8 directory

--- Lesson_8/view.py
+++
@@ -1,7 +0,0 @@
-def get_expression():
-    expression = input('Введите арифметическое выражение, используя числа и знаки + - * /: ')
-    return expression
-
-
-def print_result(origin_expression, result):
-    print(f'{" ".join(origin_expression)} = {result[0]}')
